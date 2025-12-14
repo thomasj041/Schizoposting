de90bc5051c9092dbd9cb57d06f43bdfda73d0e1
Thomas Olson <thomas.josholson@gmail.com>
2025-12-13 22:21:16 -0800

    Blink182.

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ class Script {
         setInterval(() => {
             this.spawnEyes();
             console.log("spawning eyes");
-        }, /*((Math.random() * 5000) + 1000)*/1);
+        }, ((Math.random() * 3000) + 1000));
     }
     spawnEyes() {
         const eyeBox = document.createElement('div');
@@ -11,13 +11,24 @@ class Script {
         eyeBox.style.top = Math.floor(Math.random() * window.innerHeight) + 'px';
         eyeBox.style.left = Math.floor(Math.random() * window.innerWidth) + 'px';
         const eyes = document.createElement('span');
-        eyes.innerText = '👀';
+        eyes.innerHTML = `. .`;
         eyeBox.appendChild(eyes);
         document.body.appendChild(eyeBox);
 
         setTimeout(() => {
-            eyeBox.remove();
-        }, ((Math.random() * 5000)));
+            if (Math.random() >= 0.33) {
+                eyes.style.color = 'black';
+                setTimeout(() => {
+                    eyes.style.color = 'gray';
+                }, 200);
+                setTimeout(() => {
+                    eyeBox.remove();
+                }, (Math.random() * 5000 + 1000));
+            } 
+            else {
+                eyeBox.remove();
+            }
+        }, ((Math.random() * 5000 + 500)));
     }
 }
 
